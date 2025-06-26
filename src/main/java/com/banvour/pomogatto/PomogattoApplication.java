7af1443a6b87028be44afc447afc190a45ea3e42
admin <boettcher.maja@gmail.com>
2025-06-26 21:00:11 +0200

    Initial commit: Add Spring Boot project structure

---
+++ src/main/java/com/banvour/pomogatto/PomogattoApplication.java
@@ -0,0 +1,13 @@
+package com.banvour.pomogatto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomogattoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomogattoApplication.class, args);
+	}
+
+}
